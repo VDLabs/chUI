9e1166d29a65f677b1492577bd69cf245653d78c
jakub <jakub@bryntum.com>
2012-09-27 02:23:14 +0200

    - added base structure for Core.js - added first test html page - cleared Data.js

--- chUI/Core/Core.js
+++ chUI/Core/Core.js
@@ -2,9 +2,37 @@
  * @class chUI
  * @singleton
  */
-var chUI = chUI || {};
-(function(undefined) {
-	chUI.window = this;
+var chUI =  (function(window, undefined) {
+    var chui = {},
+        C;
 
+    chui.window = window;
 
+    function isFunction(value) { return typeof value === 'function'; }
+    function isObject(value) { return typeof value === 'object'; }
+    function isArray(value) { return typeof value === 'array'; }
+    function isString(value) { return typeof value === 'string'; }
+
+    chui.sel = function (selector, context){
+        if (!selector){
+            return [];
+        } else if ( isFunction(selector)){
+
+        } else if ( isString(selector) ){
+
+        } 
+        //uzywamy wlasnych kolekcji ?
+        //else if ()
+        //uzyc contextu
+    };
+
+    C = function(selector, context){
+        return chui.sel(selector, context);
+    };
+
+    C.chui = chui;
+
+    return C;
 })();
+
+window.chUI = window.chUI || chUI;
